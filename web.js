b5b5dcb4efd59c23d7d01e1f4f1de43170045637
Yan Luo <yanluo.uml@gmail.com>
2013-07-07 17:21:31 -0400

    utf-8

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
   //response.send('Hello World 2 !');
     var buf = fs.readFileSync("index.html");
-    response.send(buf.toString('utf-9'));
+    response.send(buf.toString('utf-8'));
 
 });
 
